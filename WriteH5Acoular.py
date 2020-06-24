bd8ae829fc43eef247170690b27ec9dd27f1070c
gomeslucasm <44169749+gomeslucasm@users.noreply.github.com>
2020-06-24 16:09:48 -0300

    Create WriteH5Acoular.py

---
+++ WriteH5Acoular.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 24 15:59:53 2020
+
+@author: lucas Muller Gomes 
+Acoustic Engineegiring - UFSM
+
+"""
+import tables
+import numpy
+import acoular
+
+N_channels = # Number of channels
+fs = # Sample rating
+data = # data to save in the hdf5 - the shape must be (num_samples, number of channels)
+data = numpy.array(data)
+savename = "DataToAcoular.h5"
+
+acoularh5 = tables.open_file(savename, mode = "w", title = filename)
+acoularh5.create_earray('/','time_data', atom=None, title='', filters=None, expectedrows=100000, chunkshape=[256,N_channels], \
+                         byteorder=None, createparents=False, obj=data)
+acoularh5.set_node_attr('/time_data','sample_freq', fs)
+acoularh5.close()
+
+# Acoular data
+
+ts=acoular.TimeSamples(name= savename)
+
